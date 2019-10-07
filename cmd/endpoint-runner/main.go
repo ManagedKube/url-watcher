e57718f96dc982f6799a77da2a9ed5c9c23a3e06
garland <garlandk@gmail.com>
2019-10-06 19:24:21 -0700

    Using the customizable http client
    Signed-off-by: garland <garlandk@gmail.com>

--- cmd/endpoint-runner/main.go
+++ cmd/endpoint-runner/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"encoding/json"
 	"time"
 )
 
@@ -81,12 +81,12 @@ func testRunner(endpoint urlWatchEndpointSpec){
 	log.Println("[INFO] Starting test runner for:", endpoint.Host)
 
 	for true{
+		// Sleep
+		time.Sleep(time.Duration(endpoint.Interval) * time.Second)
+
 		log.Println("[INFO] running", endpoint.Host)
 
 		testEndpoint(endpoint)
-
-		// Sleep
-		time.Sleep(time.Duration(endpoint.Interval) * time.Second)
 	}
 
 }
@@ -106,11 +106,25 @@ func testEndpoint(endpoint urlWatchEndpointSpec){
 
 func runnerGet(endpoint urlWatchEndpointSpec){
 
-	resp, err := http.Get(endpoint.Protocol+"://"+endpoint.Host+endpoint.Path)
+	// Doc: https://golang.org/pkg/net/http/
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", endpoint.Protocol+"://"+endpoint.Host+endpoint.Path, nil)
 	if err != nil {
 		log.Println("[INFO] Test failed: ", err)
-	}else{
-		// Print the HTTP Status Code and Status Name
+	}
+
+	// Add Optional headers
+	// TODO: make this dynamic and take this config in from the ENDPOINT_TEST_JSON input
+	req.Header.Add("If-None-Match", `W/"wyzzy"`)
+
+	// Run request
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("[INFO] Test failed: ", err)
+	}else {
+
+		//Print the HTTP Status Code and Status Name
 		log.Println("HTTP Response Status:", endpoint.Host, resp.StatusCode, http.StatusText(resp.StatusCode))
 
 		if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
@@ -119,5 +133,4 @@ func runnerGet(endpoint urlWatchEndpointSpec){
 			log.Println("Argh! Broken", endpoint.Host)
 		}
 	}
-
 }
